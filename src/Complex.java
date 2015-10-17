903971db84a129b7ecebc755498e597eae0056d4
Piotrek <pstabno@gmail.com>
2015-10-17 14:57:28 +0200

    Initial commit with beggining of Complex class

---
+++ src/Complex.java
@@ -0,0 +1,41 @@
+public class Complex {
+
+	private Integer reality;
+	private Integer imaginary;
+
+	public Integer getReality() {
+		return reality;
+	}
+
+	public void setReality(Integer real) {
+		reality = real;
+	}
+
+	public Integer getImaginary() {
+		return imaginary;
+	}
+
+	public void setImaginary(Integer imagin) {
+		imaginary = imagin;
+	}
+
+	public Complex addComplex(Complex x) {
+		this.setReality(this.getReality() + x.getReality());
+		this.setImaginary(this.getImaginary() + x.getImaginary());
+		return this;
+	}
+
+	public Complex subtractComplex(Complex x) {
+		this.setReality(this.getReality() - x.getReality());
+		this.setImaginary(this.getImaginary() - x.getImaginary());
+		return this;
+	}
+
+	public Complex multiByComplex(Complex x) {
+		this.setReality((this.getReality() * x.getReality())
+				- (this.getImaginary() * x.getImaginary()));
+		this.setImaginary((this.getReality() * x.getImaginary())
+				+ (x.getReality() * this.getImaginary()));
+		return this;
+	}
+}
