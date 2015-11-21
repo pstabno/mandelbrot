7f1956584134e360c15e30c9d255f3d5a5d5c6ec
Piotrek <pstabno@gmail.com>
2015-11-21 11:20:07 +0100

    Remove Image class, new functions to Imag

--- src/sources/Imag.java
+++ src/sources/Imag.java
@@ -18,6 +18,14 @@ public class Imag {
 	public Imag(Integer x, Integer y){
 		img = new BufferedImage(x, y, BufferedImage.TYPE_BYTE_GRAY);
 	}
+
+    public void setBg(Integer c){
+        for(int i=0; i<this.img.getWidth(); i++){
+            for(int j=0; j<this.img.getHeight(); j++ ){
+                this.setPixel(i,j,c);
+            }
+        }
+    }
 	
 //	public void addBlack(Double x, Double y, Boolean c){
 //		if(c==true)
@@ -29,8 +37,24 @@ public class Imag {
 	public void setPixel(Double x, Double y, Integer col){
 		img.setRGB(Integer.parseInt(x.toString()), Integer.parseInt(y.toString()), col);
 	}
-	
-	public void saveImage() throws IOException{
+
+    public void setPixel(Integer x, Integer y, Integer col) {
+        img.setRGB(x,y,col);
+    }
+
+    public void saveImage() throws IOException{
 		ImageIO.write(img, "pbm", new File( "file.pbm"));
 	}
+
+	public Integer getSizeY(){
+		return img.getHeight();
+	}
+
+	public Integer getSizeX(){
+		return img.getWidth();
+	}
+
+    public Integer getPixel(Integer x, Integer y){
+        return img.getRGB(x,y);
+    }
 }
